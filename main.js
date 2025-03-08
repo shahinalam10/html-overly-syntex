d9eecd3ecc4ca56ad3c6f26737fb0de353d00fb9
Md Shahin Alam <64301696+shahinalam10@users.noreply.github.com>
2025-03-09 00:13:41 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+// JavaScript function
+
+//fahrenheit to Celsius
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+let x = fahrenheitToCelsius(100);
+document.write(x);
+
+// function myFunction(a,b) {
+//     return a * b;
+// } 
+// let x = myFunction(4, 3);
+// let y = myFunction(4, 4);
+// document.write(x)+'<br>';
+// document.write(y);
+
+//========================================================================================================
+
+//javascript data types
+//  string, number, boolean, object, undefined, null, symbol, bigint
+
+//string
+// let name = 'John Doe';
+// name += ' is a web developer';
+// document.write(name); 
+
+//number
+
+
+
+//========================================================================================================
+//Assignment operator
+// let x = 5;
+//     x+=5;
+//     document.write(x);
+    
+// let y = 10;
+//     y-=5;
+//     document.write(y)+'<br>';
+// let z = 2;
+//     z*=5;
+//     document.write(z)+'<br>';
+// let a = 10;
+//     a/=2;
+//     document.write(a)+'<br>';            
+
+//========================================================================================================
+// perator precedence
+// let x = 5;
+// let y = 6;
+// let z = 2;
+// let result = x + y * z;
+// document.write(result);
+
+//exponential operator
+// let x = 5;
+// z = Math.pow(x, 6);
+// document.write(z);
+
+//========================================================================================================
+// JavaScript Variables
+/*
+let carName = 'Tesla'; 
+let carModel = 'Model S';
+let carYear = 2021;
+let carPrice = 100000;
+document.getElementById('variable').innerHTML = carName + ' ' + carModel + ' ' + carYear + ' ' + carPrice;
+*/
+
+/*
+//let use 
+let city = 'New York';
+document.write(city);
+city = 'Los Angeles';
+document.write(city)+'<br>';
+*/
+
+/*
+//const use
+const country = ['USA', 'Canada', 'Mexico'];
+
+for (let i = 0; i < country.length; i++) {
+    document.write(country[i] + '<br>');
+}
+*/
